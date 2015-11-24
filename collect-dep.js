76e1824be89047f9657bdaf0ba36dfb0e244a87b
Jesse McCarthy <git_commits@jessemccarthy.net>
2015-11-24 14:51:19 -0500

    Add comments.

--- collect-dep.js
+++ collect-dep.js
@@ -1,9 +1,19 @@
+/*
+Like https://github.com/substack/node-detective, and adapted from it,
+but scope aware (though slower).
+*/
+
 var
   assign = require("object-assign"),
   babel = require("babel-core"),
   Plugin = require("babel-plugin-collect-dep"),
   word_regexes = {};
 
+/**
+ * Get / cache regex for `word`.
+ * @param string word
+ * @return RegExp
+ */
 function word_regex (word) {
   return word_regexes[word] = word_regexes[word] ||
     RegExp("\\b" + word + "\\b");
@@ -11,6 +21,12 @@ function word_regex (word) {
 
 module.exports = exports = collect_dep;
 
+/**
+ * Find deps.
+ * @param string src
+ * @param object opts?
+ * @return array Dep strings.
+ */
 function collect_dep (src, opts) {
   return find(src, opts).strings;
 }
@@ -18,6 +34,12 @@ function collect_dep (src, opts) {
 
 exports.find = find;
 
+/**
+ * Find deps.
+ * @param string src
+ * @param object opts?
+ * @return object Object with strings, expressions, nodes? arrays.
+ */
 function find (src, opts) {
   opts = assign({}, opts);
 
@@ -29,6 +51,11 @@ function find (src, opts) {
 
   if (! word_regex(word).test(src)) return deps;
 
+  /**
+   * Callback for found dep. Add to the given collection.
+   * @param string collection strings|expressions|nodes
+   * @param string val
+   */
   opts.dep = function (collection, val) {
     deps[collection].push(val);
   };
